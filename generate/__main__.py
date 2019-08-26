f1fca52e896c210b4f77daa10ef40eea35ce45f3
Oskar Lundin <c.oskar.lundin@gmail.com>
2019-08-26 19:05:54 +0200

    runnable test

--- generate/__main__.py
+++ generate/__main__.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 
+import argparse
 import generate
 
 
 def main():
-    pass
+    parser = argparse.ArgumentParser(description="Generate static website.")
+    parser.add_argument("dir", help="Website directory.")
+    
+    args = parser.parse_args()
+
+    generate.generate(args.dir)
 
 
 if __name__ == "__main__":
--- generate/generate.py
+++ generate/generate.py
@@ -1,10 +1,36 @@
+import os
 import jinja2
 
 
-def generate(website):
+def generate(path, filters=None, tests=None, globals=None, policies=None):
+    """Generate website in directory dir."""
+    # create jinja2 environment
     env = jinja2.Environment(
-        loader=jinja2.PackageLoader(website, "templates"),
+        loader=jinja2.FileSystemLoader(
+            searchpath=os.path.join(path, "templates"),
+            encoding="utf-8",
+            followlinks=True
+        ),
         autoescape=jinja2.select_autoescape(["html", "xml"])
     )
 
-    tmpl = env.get_template("base.html")
+    # https://jinja.palletsprojects.com/en/2.10.x/api/#jinja2.Environment.filters
+    if filters:
+        env.filters.update(filters)
+    
+    # https://jinja.palletsprojects.com/en/2.10.x/api/#jinja2.Environment.tests
+    if tests:
+        env.tests.update(tests)
+
+    # https://jinja.palletsprojects.com/en/2.10.x/api/#jinja2.Environment.globals
+    if globals:
+        env.globals.update(globals)
+    
+    # https://jinja.palletsprojects.com/en/2.10.x/api/#jinja2.Environment.policies
+    if policies:
+        env.globals.update(policies)
+
+    # compile templates
+    env.compile_templates(
+        target=os.path.join(path, "public")
+    )
