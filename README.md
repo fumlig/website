924450ce514b41c9c93f16db60c159071464b6a7
Oskar Lundin <c.oskar.lundin@gmail.com>
2019-08-28 00:45:09 +0200

    add serve argument to generator

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 `python3 -m generator [website] create`
 
 `python3 -m generator [website] generate`
+
+`cd website/generated`
+
+`python -m http.server 8080`
--- generator/__main__.py
+++ generator/__main__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
+import os
 import argparse
+import http.server
 
 import generator
 
@@ -19,6 +21,11 @@ def main():
     parser_generate = subparsers.add_parser("generate", help="Generate website.")
     parser_generate.set_defaults(func=generate)
 
+    # serve
+    parser_serve = subparsers.add_parser("serve", help="Serve website.")
+    parser_serve.add_argument("-p", "--port", type=int, default=8080, help="Port to serve website on.")
+    parser_serve.set_defaults(func=serve)
+
     args = parser.parse_args()
     args.func(args)
 
@@ -33,6 +40,15 @@ def generate(args):
     print("Generating website " + args.path)
     website = generator.Website(args.path, generator.Config())
     website.generate()
+
+
+def serve(args):
+    print("Serving website on " + str(args.port))
+    website = generator.Website(args.path, generator.Config())
+    os.chdir(website.generated_path)
+    server_address = ("", args.port)
+    httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
+    httpd.serve_forever()
 
 
 if __name__ == "__main__":
